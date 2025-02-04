bf27f5bef1c9520a1bc657eb43d26e13fb03e1ce
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-02-04 19:22:18 +0500

    Add stories for compact pagination component

---
+++ src/components/navigation/compactPagination/CompactPagination.stories.ts
@@ -0,0 +1,44 @@
+import CompactPagination from './CompactPagination.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import CompactPaginationItem from './CompactPaginationItem.vue';
+import { ref } from 'vue';
+
+const meta = {
+  title: 'Navigation/CompactPagination',
+  component: CompactPagination,
+  tags: ['autodocs'],
+} satisfies Meta<typeof CompactPagination>;
+
+export default meta;
+
+type Story = StoryObj<typeof CompactPagination>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { CompactPagination, CompactPaginationItem },
+    setup() {
+      const paginationItems = Array.from({ length: 8 }, (_, i) => i + 1);
+      const selected = ref(1);
+      return { args, paginationItems, selected };
+    },
+    template: `
+    <div :style="{
+      display: 'inline-flex',
+      alignItems: 'center',
+      padding: '20px',
+      background: args.mode === 'white' ? 'var(--tgui-black)' : 'var(--tgui-secondary-bg-color)'
+    }">
+      <CompactPagination v-bind="args">
+        <CompactPaginationItem
+          v-for="item of paginationItems"
+          :key="item"
+          :selected="item === selected"
+          @click="selected = item"
+        >
+          {{ item }}
+        </CompactPaginationItem>
+      </CompactPagination>
+    </div>
+    `,
+  }),
+};
