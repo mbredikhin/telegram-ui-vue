8ba8ed74be300b2848fd09c468373a5355219a03
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-02-04 22:37:54 +0500

    Add link component

---
+++ src/components/navigation/link/Link.vue
@@ -0,0 +1,12 @@
+<template>
+  <a class="link"><slot /></a>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.link {
+  text-decoration: none;
+  color: var(--tgui-link-color);
+}
+</style>
