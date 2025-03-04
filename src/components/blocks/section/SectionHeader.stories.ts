2f8b65bfb1f524554140cf9358db374c75b490ca
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-03-04 15:32:09 +0500

    Add stories for section header component

---
+++ src/components/blocks/section/SectionHeader.stories.ts
@@ -0,0 +1,27 @@
+import SectionHeader from './SectionHeader.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta = {
+  title: 'Blocks/Section/SectionHeader',
+  component: SectionHeader,
+  tags: ['autodocs'],
+} satisfies Meta<typeof SectionHeader>;
+
+export default meta;
+
+type Story = StoryObj<typeof SectionHeader>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { SectionHeader },
+    setup() {
+      return { args };
+    },
+    template: `
+    <div>
+      <SectionHeader v-bind="args">${args.default || 'Usual title'}</SectionHeader>
+      <SectionHeader large v-bind="args">${args.default || 'Large title'}</SectionHeader>
+    </div>`,
+  }),
+  args: {},
+};
