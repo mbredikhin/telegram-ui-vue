28760670762743b1d40eeba88c3a985708ab6f2d
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-04-09 11:20:28 +0500

    Add stories for rating component

---
+++ src/components/form/rating/Rating.stories.ts
@@ -0,0 +1,54 @@
+import { Section, SectionHeader } from '@/components/blocks';
+import Rating from './Rating.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import Heart28Icon from '@/icons/28/heart.svg';
+import { Component } from 'vue';
+import { Text } from '@/components/typography';
+
+const meta = {
+  title: 'Form/Rating',
+  component: Rating,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Rating>;
+
+export default meta;
+
+type Story = StoryObj<typeof Rating>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { Section, SectionHeader, Rating, Text },
+    setup() {
+      return { args };
+    },
+    template: `
+    <Section>
+      <template #header>Navigate with keyboard</template>
+      <template #footer>Use the keyboard to navigate between the stars and also click on them</template>
+      <Rating v-bind="args" />
+    </Section>`,
+  }),
+  args: {},
+};
+
+export const WithCustomIcon: Story = {
+  render: (args) => ({
+    components: {
+      Section,
+      Rating,
+      Heart28Icon: Heart28Icon as unknown as Component,
+    },
+    setup() {
+      return { args };
+    },
+    template: `
+    <Section>
+      <template #header>We use custom icon here</template>
+      <Rating v-bind="args" />
+    </Section>`,
+  }),
+  args: {
+    icon: Heart28Icon as unknown as Component,
+  },
+};
