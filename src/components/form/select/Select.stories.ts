1b8f0b526bc73ee9578dc4067dea45979a0504a3
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-04-12 22:15:06 +0500

    Add stories for select component

---
+++ src/components/form/select/Select.stories.ts
@@ -0,0 +1,32 @@
+import { List } from '@/components/blocks';
+import Select from './Select.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta = {
+  title: 'Form/Select',
+  component: Select,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Select>;
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { List, Select },
+    setup() {
+      return { args };
+    },
+    template: `
+    <List style="width: 240px; background: var(--tgui-secondary-bg-color);">
+      <Select placeholder="I am usual input, just leave me alone">
+        <template #header>Select</template>
+        <template #default>
+          <option>Hello</option>
+          <option>Okay</option>
+        </template>
+      </Select>
+    </List>`,
+  }),
+};
