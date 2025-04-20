71f3adf731986f7fd9703e331d2cc63853020f33
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-04-20 23:41:29 +0500

    Add stories for cell info component

--- src/components/blocks/cell/Cell.stories.ts
+++ src/components/blocks/cell/Cell.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/vue3';
 import Cell from './Cell.vue';
 import Badge from '../badge/Badge.vue';
 import Avatar from '../avatar/Avatar.vue';
+import Info from './info/Info.vue';
 
 const meta = {
   title: 'Blocks/Cell/Cell',
@@ -33,6 +34,28 @@ export const Default: Story = {
       </template>
       <template #after>
         <Badge type="number">99</Badge>
+      </template>
+    </Cell>
+    `,
+  }),
+  args: {},
+};
+
+export const WithInfo: Story = {
+  render: (args) => ({
+    components: { Cell, Avatar, Info },
+    setup() {
+      return { args };
+    },
+    template: `
+    <Cell v-bind="args">
+      <template #default>Noah</template>
+      <template #subtitle>Yesterday</template>
+      <template #before>
+        <Avatar size="48" />
+      </template>
+      <template #after>
+        <Info type="text" subtitle="Received">+1000</Info>
       </template>
     </Cell>
     `,
---
+++ src/components/blocks/cell/info/Info.stories.ts
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import Info from './Info.vue';
+import AvatarStack from '../../avatarStack/AvatarStack.vue';
+import Avatar from '../../avatar/Avatar.vue';
+
+const meta = {
+  title: 'Blocks/Cell/Info',
+  component: Info,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Info>;
+
+export default meta;
+
+type Story = StoryObj<typeof Info>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: { Info, Avatar },
+    setup() {
+      return { args };
+    },
+    template: `
+    <div style="display: flex; justify-content: center;">
+      <Info v-bind="args">
+        <template #default>Action</template>
+        <template #subtitle>Subtitle</template>
+      </Info>
+    </div>
+    `,
+  }),
+  args: {
+    type: 'text',
+  },
+};
+
+export const WithAvatarStack: Story = {
+  render: (args) => ({
+    components: { Info, AvatarStack, Avatar },
+    setup() {
+      return { args };
+    },
+    template: `
+    <Info type="avatarStack">
+      <template #avatar-stack>
+        <AvatarStack>
+          <Avatar size="28" />
+          <Avatar size="28" />
+          <Avatar size="28" />
+        </AvatarStack>
+      </template>
+      <template #default>
+        Action
+      </template>
+    </Info>
+    `,
+  }),
+  args: {
+    type: 'avatarStack',
+  },
+};
