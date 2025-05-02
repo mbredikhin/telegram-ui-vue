665ec8fbf50cf4abfe77ea805b9d5f3c64823f99
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-05-02 16:32:58 +0500

    Add stories for snackbar component

---
+++ src/components/feedback/snackbar/Snackbar.stories.ts
@@ -0,0 +1,74 @@
+import Snackbar from './Snackbar.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import Archive28Icon from '@/icons/28/archive.svg';
+import { Component, ref } from 'vue';
+import SnackbarButton from './SnackbarButton.vue';
+import { Link } from '@/components/navigation';
+import { Button } from '@/components/blocks';
+
+const meta = {
+  title: 'Feedback/Snackbar',
+  component: Snackbar,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Snackbar>;
+
+export default meta;
+
+type Story = StoryObj<typeof Snackbar>;
+
+export const Default: Story = {
+  render: (args) => ({
+    components: {
+      Snackbar,
+      SnackbarButton,
+      Button,
+      Link,
+      Archive28Icon: Archive28Icon as unknown as Component,
+    },
+    setup() {
+      const isDeleteSnackbarShown = ref(false);
+      const isUndoSnackbarShown = ref(false);
+
+      return {
+        args,
+        isDeleteSnackbarShown,
+        isUndoSnackbarShown,
+      };
+    },
+    template: `
+    <Button size="m" @click="isDeleteSnackbarShown = true">Show snackbar</Button>
+    <Snackbar
+      v-if="isDeleteSnackbarShown"
+      v-bind="args"
+      @close="isDeleteSnackbarShown = false"
+    >
+      <template #before>
+        <Archive28Icon />
+      </template>
+      <template #after>
+        <SnackbarButton @click="isUndoSnackbarShown = true">
+          Undo
+        </SnackbarButton>
+      </template>
+      <template #description>
+        Restore the message within 4 seconds
+      </template>
+      Message deleted
+    </Snackbar>
+      
+    <Snackbar
+      v-if="isUndoSnackbarShown"
+      v-bind="args"
+      @close="isUndoSnackbarShown = false"
+    >
+      <template #description>
+        Message returned to the list
+      </template>
+      <template #link>
+        <Link href="https://telegram.org" target="_blank">Open</Link>
+      </template>
+      Message restored
+    </Snackbar>`,
+  }),
+  args: {},
+};
