1511c9212c0a692f1efd896e8f893b777b2a5f3f
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-05-26 20:05:38 +0500

    feat: configure the release process

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,55 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { checker } from 'vite-plugin-checker';
 import path, { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import svgLoader from 'vite-svg-loader';
+import pkg from './package.json';
+import dts from 'vite-plugin-dts';
+import { argv } from 'process';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['umd'],
-      fileName: 'index',
-      name: 'telegram-ui-vue',
+export default defineConfig(() => {
+  const isStoriesBuild = argv[1].includes('storybook');
+
+  return {
+    plugins: [
+      vue(),
+      checker({
+        typescript: true,
+      }),
+      svgLoader(),
+      ...(isStoriesBuild
+        ? []
+        : [
+            dts({
+              tsconfigPath: 'tsconfig.app.json',
+              cleanVueFileName: true,
+              rollupTypes: true,
+              include: [path.resolve(__dirname, './src')],
+            }),
+          ]),
+    ],
+    resolve: {
+      alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
     },
-    target: 'esnext',
-    outDir: './dist',
-  },
-  plugins: [
-    vue(),
-    checker({
-      typescript: true,
-    }),
-    svgLoader(),
-  ],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
-  },
+    build: {
+      minify: false,
+      target: 'esnext',
+      sourcemap: true,
+      outDir: './dist',
+      lib: {
+        name: 'telegram-ui-vue',
+        fileName: 'index',
+        entry: resolve(__dirname, 'src/index.ts'),
+        formats: ['es'],
+      },
+      rollupOptions: {
+        external: [...Object.keys(pkg.dependencies ?? {})],
+      },
+    },
+  } as UserConfig;
 });
