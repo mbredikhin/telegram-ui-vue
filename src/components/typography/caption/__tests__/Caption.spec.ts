2970053bfab8a2834cde3c7906572a6cea00a01d
Maxim Bredikhin <mbredikhin.dev@gmail.com>
2025-08-25 10:54:09 +0500

    test: add tests for Caption

---
+++ src/components/typography/caption/__tests__/Caption.spec.ts
@@ -0,0 +1,54 @@
+import { mount, randomString } from '@/lib/tests';
+import Caption from '../Caption.vue';
+
+describe('Caption', () => {
+  test('renders with default props', () => {
+    const text = randomString();
+    const wrapper = mount(Caption, {
+      slots: {
+        default: text,
+      },
+    });
+
+    const root = wrapper.findComponent({ name: 'Typography' });
+
+    expect(root.exists()).toBe(true);
+    expect(root.text()).toBe(text);
+
+    expect(root.props('is')).toBe('span');
+    expect(root.props('weight')).toBe('3');
+
+    expect(root.classes()).toContain('caption');
+    expect(root.classes()).toContain('caption--1');
+  });
+
+  test('applies level correctly', () => {
+    const wrapper = mount(Caption, {
+      props: {
+        level: '2',
+      },
+    });
+
+    const root = wrapper.findComponent({ name: 'Typography' });
+
+    expect(root.classes()).toContain('caption--2');
+    expect(root.classes()).not.toContain('caption--1');
+  });
+
+  test('forwards extra attrs to Typography', () => {
+    const testId = randomString();
+    const dataTest = randomString();
+
+    const wrapper = mount(Caption, {
+      attrs: {
+        id: testId,
+        'data-test': dataTest,
+      },
+    });
+
+    const root = wrapper.findComponent({ name: 'Typography' });
+
+    expect(root.attributes('id')).toBe(testId);
+    expect(root.attributes('data-test')).toBe(dataTest);
+  });
+});
